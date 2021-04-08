080c4d2c30f77f71a818f0a0b65242392561e018
Taylor Griffin <tmgriffin73@gmail.com>
2021-04-07 23:17:17 -0400

    refactoring

--- Creative Single Page Site/script.js
+++ Creative Single Page Site/script.js
@@ -7,21 +7,17 @@ const firstSlide= document.querySelector('.slide_1');
 const secondSlide= document.querySelector('.slide_2');
 const thirdSlide= document.querySelector('.slide_3');
 
-
-
 const slides= document.querySelectorAll('figure');
 
-
-
-
-
-
+// Nav hide/show
 burger.addEventListener('click', ()=>{
 burger.classList.toggle('hide');
 close.classList.toggle('hide');
 navMenu.classList.toggle('hide');
 navMenu.classList.toggle('show');
 })
+
+//Image slider
 
 function showNextImage(e) {
  
@@ -55,36 +51,27 @@ function showNextImage(e) {
     el.classList.toggle('hide-slide');
   }
 
+ 
+
 
 prev.addEventListener('click', showPrevImage);
 next.addEventListener('click', showNextImage);
 
+// Add copyright year
+const footer=document.querySelector('footer');
+const smallCopy= document.createElement('p');
+smallCopy.innerHTML=`
+Coded ${new Date().getFullYear()} by Taylor Griffin from a Frontend Mentor Project
+`;
+smallCopy.style.cssText='font-size:12px; text-align:center; margin-bottom:5px';
+footer.insertAdjacentElement("afterend", smallCopy);
+
+
 	
 	
 	
 
-// prev.addEventListener('click', ()=>{
-//     slides.forEach((slide, i)=>{
-//         if(slide[i].previousElementSibling){
-           
-//             slide.classList.toggle('hide-slide')
-//         }else{
-//             slides[2].classList.remove('hide-slide')
-          
-//         }
-//     })
-   
-// })
-// next.addEventListener('click', ()=>{
-//   slides.forEach(slide=>{
-//       if(slide.nextElementSibling){
-//           slide.nextElementSibling.classList.toggle('hide-slide')
-//       }else{
-//           slide[0].classList.toggle('hide-slide')
-//       }
-//   })
 
-// })
 
 
 
